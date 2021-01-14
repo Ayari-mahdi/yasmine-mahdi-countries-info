c255e4cd08a52cfd04961ec3b2ed37643f4f165d
mahdi <mahdi.ayari@myu.universitecentrale.tn>
2021-01-14 18:37:03 +0100

    project

--- src/app/ANGULAR-PROJECT/countries-card/countries-card.component.ts
+++ src/app/ANGULAR-PROJECT/countries-card/countries-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit,  } from '@angular/core';
-import { Router, Routes } from '@angular/router';
+import { Router} from '@angular/router';
 import { Countries } from '../model/Countries';
 
 @Component({
--- src/app/ANGULAR-PROJECT/header/header.component.ts
+++ src/app/ANGULAR-PROJECT/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { SVcountryService } from '../svcountry.service';
+
 
 @Component({
   selector: 'app-header',
